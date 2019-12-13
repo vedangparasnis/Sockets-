0bb94707bd8e97469b3bb4da9f0751306e58c839
vedangparasnis <35874709+vedangparasnis@users.noreply.github.com>
2019-12-13 19:10:35 +0530

    config changes

--- configs/firebase.js
+++ configs/firebase.js
@@ -2,14 +2,14 @@ const firebase = require("firebase-admin");
 const functions = require("firebase-functions");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXmr78sx4ukNCAK3cdybNKIUM-Gk40_10",
-  authDomain: "chat-r-51adc.firebaseapp.com",
-  databaseURL: "https://chat-r-51adc.firebaseio.com",
-  projectId: "chat-r-51adc",
-  storageBucket: "chat-r-51adc.appspot.com",
-  messagingSenderId: "147877636139",
-  appId: "1:147877636139:web:3f388ad93231ecc408226e",
-  measurementId: "G-CBCWSBE27P"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "c",
+  storageBucket: "x",
+  messagingSenderId: "x",
+  appId: "1:147877636139:",
+  measurementId: "x"
 };
 
 module.exports.config = firebaseConfig;
